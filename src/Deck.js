f435ee6ae6ffb5df6d21918af32a0d6002eb9bba
Nathan Newman <lordnate.moose@gmail.com>
2023-02-13 18:04:33 -0600

    added Deck component

---
+++ src/Deck.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Deck = () => {
+  const [deck, setDeck] = useState(null);
+  const [card, setCard] = useState(null);
+
+  useEffect(() => {
+    async () => {
+      const res = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+      setDeck(res.data.deck_id);
+    };
+  }, [setDeck]);
+
+  useEffect(() => {
+    async () => {
+        const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deck}/draw/?count=1`);
+        setCard(res.data);
+    }
+  });
+
+  return (
+    <div>
+        {deck ? <button onClick={setCard}>Get Deck</button> : <button onClick={setDeck}>Draw Card</button>}
+            
+    </div>
+  );
+};
